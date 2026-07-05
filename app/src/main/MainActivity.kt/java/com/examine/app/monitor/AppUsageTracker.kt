490a3ae6dd66239086a188383d4931f09c31ae48
bjakbn <bjak6286@gmail.com>
2026-07-06 04:15:58 +0800

    Update AppUsageTracker.kt

--- app/src/main/MainActivity.kt/java/com/examine/app/monitor/AppUsageTracker.kt
+++ app/src/main/MainActivity.kt/java/com/examine/app/monitor/AppUsageTracker.kt
@@ -1,1 +1,28 @@
 
+package com.examine.app.monitor
+
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
+import android.content.Context
+
+class AppUsageTracker(private val context: Context) {
+
+    fun getForegroundApp(): String? {
+        val usm = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+        val time = System.currentTimeMillis()
+        val events = usm.queryEvents(time - 10000, time)
+
+        val event = UsageEvents.Event()
+        var lastPkg: String? = null
+
+        while (events.hasNextEvent()) {
+            events.getNextEvent(event)
+            if (event.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                lastPkg = event.packageName
+            }
+        }
+
+        return lastPkg
+    }
+}
